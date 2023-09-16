6ca90be23c0403768592c6c31d1b5ef1a0a9b756
MAbdurahman <mdbdrrhm@gmail.com>
2023-09-15 21:44:23 -0500

    Add preloader, its styles and scripts; Add images for home background

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
-$(function() {
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+    // makes sure that whole site is loaded
+    $('#preloader-gif, #preloader').fadeOut(5000, function () {
+    });
+});
+
+/*=============================================
+          js-navbar-animated
+================================================*/
+$(function () {
     console.log('The DOM is ready to be used.');
-})
+});
